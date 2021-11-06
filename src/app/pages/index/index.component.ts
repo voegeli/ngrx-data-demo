22821e477601f00a6506438655e88b2cd3223a82
voegeli <peter.voegeli@gmx.net>
2021-11-06 23:38:07 +0100

    Cleanup

--- src/app/pages/index/index.component.ts
+++ src/app/pages/index/index.component.ts
@@ -4,6 +4,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { PostEntityService } from 'src/app/data/post-entity.service';
 import { Post } from 'src/app/models/Post';
+import { setSelectedPost } from 'src/app/state/app.actions';
 import { AppState } from 'src/app/state/app.state';
 
 @Component({
@@ -13,10 +14,10 @@ import { AppState } from 'src/app/state/app.state';
 })
 export class IndexComponent implements OnInit {
 
-    posts: Observable<Post[]>;
+    posts$: Observable<Post[]>;
 
     constructor(private _store: Store<AppState>, private _postService: PostEntityService, private _router: Router) {
-        this.posts = this._postService.entities$;
+        this.posts$ = this._postService.entities$;
     }
 
     ngOnInit(): void {
@@ -31,5 +32,10 @@ export class IndexComponent implements OnInit {
 
     onAddNewPost() {
         this._router.navigate(['create'])
+    }
+
+
+    onSelectPost(post: Post) {
+        this._store.dispatch(setSelectedPost({post}));
     }
 }
